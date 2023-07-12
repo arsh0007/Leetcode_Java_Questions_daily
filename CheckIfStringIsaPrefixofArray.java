edd7d776202d9abdd2cd545a1c21eeb30466b6a5
Arsh Kumar <85900453+arsh0007@users.noreply.github.com>
2023-07-12 23:13:39 +0530

    Create CheckIfStringIsaPrefixofArray.java
    Given a string s and an array of strings words, determine whether s is a prefix string of words.
    
    A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
    
    Return true if s is a prefix string of words, or false otherwise.
    
     
    
    Example 1:
    
    Input: s = "iloveleetcode", words = ["i","love","leetcode","apples"]
    Output: true
    Explanation:
    s can be made by concatenating "i", "love", and "leetcode" together.
    Example 2:
    
    Input: s = "iloveleetcode", words = ["apples","i","love","leetcode"]
    Output: false
    Explanation:
    It is impossible to make s using a prefix of arr.
     
    
    Constraints:
    
    1 <= words.length <= 100
    1 <= words[i].length <= 20
    1 <= s.length <= 1000
    words[i] and s consist of only lowercase English letters.

---
+++ CheckIfStringIsaPrefixofArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        StringBuilder prefix = new StringBuilder();
+
+        for(String word: words )
+        {
+            prefix.append(word);
+
+            if(prefix.toString().equals(s))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
